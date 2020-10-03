5bcb0f6a9e186444c37bac8241a91b2df647e136
gtfn <71272263+gtfn@users.noreply.github.com>
2020-10-03 21:58:04 +0200

    Add files via upload

--- signalgenerator_rechteck.ino
+++ signalgenerator_rechteck.ino
@@ -2,6 +2,14 @@
 // Frequenz ueber Encoder einstellbar
 
 #include <Encoder.h>
+#include <Wire.h>
+#include "rgb_lcd.h" // Grove i2c lcd
+
+rgb_lcd lcd;
+
+const int colorR = 255;
+const int colorG = 0;
+const int colorB = 0;
 
 // Change these two numbers to the pins connected to your encoder.
 //   Best Performance: both pins have interrupt capability
@@ -12,21 +20,30 @@ Encoder myEnc(3, 4);
 
 unsigned int frequenz = 1000;
 unsigned int f_min = 30;
-unsigned int f_max =65535;
+unsigned int f_max = 65535;
 
 void setup() {
   // put your setup code here, to run once:
   //tone(10, frequenz);
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  lcd.setRGB(colorR, colorG, colorB);
+  // Print a message to the LCD.
+  lcd.print("hello, world!");
+
+  delay(1000);
 }
 
 long oldPosition  = -999;
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
   long newPosition = myEnc.read();
   if (newPosition != oldPosition) {
     oldPosition = newPosition;
-    //Serial.println(newPosition);
+    lcd.print(newPosition);
     tone(10, constrain(newPosition, f_min, f_max));
   }
 }
